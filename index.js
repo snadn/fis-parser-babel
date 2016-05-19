3a582636f295035e4eafb9cb2d5f9d33dd30764f
snadn <snadn@snadn.cn>
2016-05-19 18:15:54 +0800

    添加 filename 参数

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 var babel = require("babel-core");
 
-module.exports = function(content, file, conf){
+module.exports = function(content, file, conf) {
 	// console.log(file);
+	var opt = fis.util.merge({
+		filename: file.realpath
+	}, conf.options);
+
 	if (fis.util.filter(file.subpath, conf.include, conf.exclude)) {
 		var s = babel.transform(content, conf.options);
 		// console.log(s.code);
