a619c6cc4f76ebedc4530bc3bfefee8cc2ccdf08
snadn <snadn@snadn.cn>
2018-10-08 19:21:25 +0800

    feat: BRACKCHANGE: support babel v7

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+### 4.0.0
+
+- update babel/core dependencies to 7.0.0
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var babel = require("babel-core");
+const babel = require("@babel/core");
 
 module.exports = function(content, file, conf) {
 	// console.log(file);
-	var opt = {
+	const opt = {
 		filename: file.realpath
 	};
 	fis.util.merge(opt, conf.options);
 
 	if (fis.util.filter(file.subpath, conf.include, conf.exclude)) {
-		var s = babel.transform(content, opt);
+		const s = babel.transform(content, opt);
 		// console.log(s.code);
 		return s.code;
 	} else {
